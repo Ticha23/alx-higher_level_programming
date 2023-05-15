8f597e2ce84ee70c04a309e83fb2a624c4bb7c26
Ticha23 <mutwirigachanja1@gmail.com>
2023-05-15 12:13:54 -0700

    what we are doing is now changing the element in a list and printing the new list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    else:
+        new_l = list(my_list)
+        new_l[idx] = element
+        return new_l
