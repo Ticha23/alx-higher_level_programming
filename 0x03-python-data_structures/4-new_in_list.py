11446b4a9c08cc1a15524a1b717744706ba32ec7
Ticha23 <mutwirigachanja1@gmail.com>
2023-05-15 12:27:16 -0700

    trying to do a interchange

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,8 @@
+#!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0:
-        return my_list
-    elif idx >= len(my_list):
-        return my_list
+    copy = my_list.copy()
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list.copy()
     else:
-        new_l = list(my_list)
-        new_l[idx] = element
-        return new_l
+        copy[idx] = element
+        return copy
