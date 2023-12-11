6005b37d3567f6e3746ea4cb5b5e98dce9eaf03b
Ticha23 <mutwirigachanja1@gmail.com>
2023-12-11 16:32:23 +0300

    my js first file

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,8 @@
-#!/usr/bin/env node
-const myVar = 'JavaScript is amazing';
+#!/usr/bin/node
+
+// Create a constant variable called myVar
+const myVar = "JavaScript is amazing";
+
+// Use console.log(...) to print the output
 console.log(myVar);
+
