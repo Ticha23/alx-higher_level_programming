88d97ff4d45476637484226ae202c57b1affc367
Ticha23 <mutwirigachanja1@gmail.com>
2023-12-14 01:10:01 +0300

    my concatenation file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-const numArgs = process.argv.length - 2; // Subtracting 2 to exclude 'node' and the script filename
-if (numArgs === 0) {
+if (process.argv.length === 2) {
   console.log('No argument');
-} else if (numArgs === 1) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
-
